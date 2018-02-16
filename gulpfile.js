75a724b6e12b1572caf66ef7f615ddc6733cdf11
Alex Arriaga <alex.arriaga.m@gmail.com>
2018-02-15 19:36:01 -0600

    Agregar ejemplo de Gulp.js tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,25 @@ const sourcemaps = require('gulp-sourcemaps');
 const gutil = require('gulp-util');
 const autoprefixer = require('gulp-autoprefixer');
 
+gulp.task('watch', () => {
+  gulp.watch(['src/scss/**/*.scss'], ['sass:compiled']);
+});
+
 gulp.task('sass:compiled', () => {
   function buildStyles(prod) {
     return gulp.src('src/scss/main.scss').pipe(sourcemaps.init()).pipe(sass({
-      outputStyle: prod ? 'compressed' : 'expanded'
+      outputStyle: prod ? 'compressed' : 'expanded',
     })).pipe(rename(filePath => {
           if (filePath.basename === 'main') {
             filePath.basename = 'all-styles';
           }
           if (prod) {
-            filePath.basename = `.min${filePath.basename}`;
+            filePath.extname = `.min${filePath.extname}`;
           }
-        })
+        }),
     ).pipe(sourcemaps.write('.', {
       includeContent: false,
-      sourceRoot: '../src'
+      sourceRoot: '../src',
     })).pipe(gulp.dest('dist/css'));
   }
 
